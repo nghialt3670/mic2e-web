f32eb9105995db10454a0c3d827091c3d3ac197a
nghialt3670 <nghialt3670@gmail.com>
2025-10-24 23:55:54 +0700

    feat: add motion

--- src/features/chat/components/upload-attachment-list/upload-attachment-list.tsx
+++ src/features/chat/components/upload-attachment-list/upload-attachment-list.tsx
@@ -1,17 +1,28 @@
 import { useUploadAttachmentStore } from "../../stores/upload-attachment-store";
 import { UploadAttachmentItem } from "../upload-attachment-item";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const UploadAttachmentList = () => {
   const { files } = useUploadAttachmentStore();
 
   return (
     <div className="flex flex-wrap gap-2 pb-2">
-      {files.map((file) => (
-        <UploadAttachmentItem
-          key={file.name}
-          file={file}
-        />
-      ))}
+      <AnimatePresence mode="popLayout">
+        {files.map((file) => (
+          <motion.div
+            key={file.name}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.2 }}
+            layout
+          >
+            <UploadAttachmentItem
+              file={file}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
