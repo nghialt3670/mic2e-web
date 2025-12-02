f132c53b604c8a7424c61a23afe0e2c2c4b9daf6
nghialt3670 <nghialt3670@gmail.com>
2025-12-02 21:20:53 +0700

    fix: add not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-full w-full items-center justify-center text-sm text-muted-foreground">
+      <p>Sorry, this page could not be found.</p>
+    </div>
+  );
+}
+
+
